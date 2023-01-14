9590a71613bdf2d3cc652e6e8ca1c761c5fb0b4c
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-14 15:54:13 -0300

    Making vibrations with UINotificationFeedbackGenerator

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,39 +5,67 @@
 //  Created by Orlando Moraes Martins on 14/01/23.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
+       Text("Hello World")
+            .padding()
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture {
+                complexSuccess()
             }
-            .onEnded { _ in
-                withAnimation{
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
         
-        let pressGesture = LongPressGesture()
-            .onEnded{ value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
+        do {
+            engine = try CHHapticEngine()
+            
+            try engine?.start()
+        } catch {
+            print("There are a error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        var events = [CHHapticEvent]()
         
-        let combined = pressGesture.sequenced(before: dragGesture)
+        for i in stride(from: 0, through: 1, by: 0.1){
+            let intensity = CHHapticEventParameter (parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter (parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append (event)
+        }
         
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        for i in stride(from: 0, through: 1, by: 0.1){
+            let intensity = CHHapticEventParameter (parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter (parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append (event)
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to player that pattern \(error.localizedDescription)")
+        }
     }
 }
 
