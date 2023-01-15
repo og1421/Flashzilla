2bc79820ec49c052f8539d89ba23badc82f0b4fd
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-15 17:00:24 -0300

    Using Reduce Transparency

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,26 +7,17 @@
 
 import SwiftUI
 
-func withOptionalAnimation <Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     @State private var scale = 1.0
     
     var body: some View {
         Text("Hello World")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAnimation{
-                    scale *= 1.5
-                }
-            }
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
     }
 }
 
