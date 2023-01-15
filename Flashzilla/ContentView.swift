e0010b997f83de3c6bd61ca0f183cf4da2cfccb7
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-15 16:42:03 -0300

    Monitoring when the app is active, inactive or running in the background

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,22 +5,22 @@
 //  Created by Orlando Moraes Martins on 14/01/23.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+    @Environment(\.scenePhase) var scenePhase
     
     var body: some View {
         Text("Hello World")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("Current time \(time)")
+            .padding()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Innactive")
+                } else if newPhase == .background {
+                    print("Background")
                 }
-                counter += 1
             }
     }
 }
