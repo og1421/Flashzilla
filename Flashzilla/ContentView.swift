0bd57997888a56d40307910af56f32b6cae72b0a
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-17 21:50:42 -0300

    Ending the app with allowHItTesting

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -49,6 +49,15 @@ struct ContentView: View {
                             .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty{
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
             }
             if differentiateWithoutColor {
                 VStack{
@@ -82,7 +91,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) { newPhase in
             if newPhase == .active {
-                isActive = true
+                if cards.isEmpty == false {
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -91,6 +102,16 @@ struct ContentView: View {
     
     func removeCard (at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty{
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array<Card>(repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
